3d0d5c97bfaa150fdf830154a2882f120a1c94d5
renaldinoviandi0@gmail.com <panjangkalijoki@gmail.com>
2022-11-21 08:43:41 +0700

    create controller file

---
+++ controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let response = require('./res');
+let connection = require('./koneksi');
+
+exports.index = function (req, res) {
+    response.ok("Aplikasi Rest Api ku berjalan",)
+}
