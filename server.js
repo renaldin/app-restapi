d80b8fb254582b48b3722bf7d797beb2ec8c685b
renaldinoviandi0@gmail.com <panjangkalijoki@gmail.com>
2022-11-22 09:17:57 +0700

    setting routes in server.js

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// parse application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// call routes 
+let routes = require('./routes');
+routes(app);
+
 app.listen(5000, () => {
     console.log(`Server started on 5000`);
 });
