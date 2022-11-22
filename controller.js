b8c44314ea70b691427c1513113f4955f21ee725
renaldinoviandi0@gmail.com <panjangkalijoki@gmail.com>
2022-11-22 20:22:50 +0700

    show all student data succesfully

--- controller.js
+++ controller.js
@@ -4,5 +4,17 @@ let response = require('./res');
 let connection = require('./koneksi');
 
 exports.index = function (req, res) {
-    response.ok("Aplikasi Rest Api ku berjalan",res)
+    response.ok("Aplikasi Rest Api ku berjalan", res)
 }
+
+// show all student data 
+
+exports.showAllStudent = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+};
--- routes.js
+++ routes.js
@@ -1,8 +1,11 @@
 'use strict';
 
-module.exports = function(app){
+module.exports = function (app) {
     let jsonku = require('./controller');
 
     app.route('/')
         .get(jsonku.index);
+
+    app.route('/show')
+        .get(jsonku.showAllStudent);
 };
