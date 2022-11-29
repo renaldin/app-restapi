88691f0b9184d1449e45ee185502feb7d0a15693
renaldinoviandi0@gmail.com <panjangkalijoki@gmail.com>
2022-11-29 07:23:08 +0700

    add controller register

---
+++ middleware/auth.js
@@ -0,0 +1,44 @@
+let ip = require('ip');
+let mysql = require('mysql');
+let md5 = require('md5');
+let response = require('../res');
+let jwt = require('jsonwebtoken');
+let config = require('../config/secret');
+let connection = require('../koneksi');
+
+// controller register 
+exports.registrasi = function (req, res) {
+    let post = {
+        username: req.body.username,
+        email: req.body.email,
+        password: md5(req.body.password),
+        role: req.body.role,
+        tanggal_daftar: new Date()
+    };
+
+    let query = "SELECT email FROM ?? WHERE ??";
+    let table = ["user", "email", post.email];
+
+    query = mysql.format(query, table);
+
+    connection.query(query, function (error, rows) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (rows.length == 0) {
+                let query = "INSERT INTO ?? SET ?";
+                let table = ["user"];
+                query = mysql.format(query, table);
+                connection.query(query, post, function (error, rows) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        response.ok("Add new user succesfully", res);
+                    }
+                });
+            } else {
+                response.ok("Email already exist!");
+            }
+        }
+    });
+}
